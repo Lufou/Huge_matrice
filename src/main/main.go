69b727d0b382db1ca29da4fbb20cf4be035c9346
Lufou <lucas.damase@laposte.net>
2021-10-08 09:50:54 +0200

    Update main.go

--- src/main/main.go
+++ src/main/main.go
@@ -1,1 +1,15 @@
 package main
+
+var TAILLE_MATRICE_A int = 10000
+var TAILLE_MATRICE_B int = TAILLE_MATRICE_A
+var matA [][]int
+var matB [][]int
+
+func main() {
+	matA := make([][]int, TAILLE_MATRICE_A)
+	matB := make([][]int, TAILLE_MATRICE_B)
+
+	//fonction pour multiplier les matrices, basique
+	// après, la même avec des coroutines
+
+}
