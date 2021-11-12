dc98f2c584fc086caa8f94394e13705c49da41c4
Lufou <lucas.damase@laposte.net>
2021-11-12 17:35:40 +0100

    Update main.go

--- src/main/main.go
+++ src/main/main.go
@@ -3,9 +3,19 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
+	"time"
 )
 
-func remplirMatrices(matList [][][]int, nombre int, sizes [][]int) {
+var wg sync.WaitGroup
+var result [][]int
+var matA [][]int
+var matB [][]int
+
+const LARGEUR_MATRICES = 1000
+const HAUTEUR_MATRICES = 1000
+
+/*func remplirMatrices(matList [][][]int, nombre int, sizes [][]int) {
 	for k := 0; k < nombre; k++ {
 		mat := make([][]int, sizes[k][0])
 		for i := 0; i < len(mat); i++ {
@@ -15,6 +25,24 @@ func remplirMatrices(matList [][][]int, nombre int, sizes [][]int) {
 			}
 		}
 		matList[k] = mat
+	}
+}*/
+
+func remplirMatrices() {
+	matA = make([][]int, HAUTEUR_MATRICES)
+	for i := 0; i < HAUTEUR_MATRICES; i++ {
+		matA[i] = make([]int, LARGEUR_MATRICES)
+		for j := 0; j < LARGEUR_MATRICES; j++ {
+			matA[i][j] = rand.Intn(100) // Default: 10000
+		}
+	}
+
+	matB = make([][]int, HAUTEUR_MATRICES)
+	for i := 0; i < HAUTEUR_MATRICES; i++ {
+		matB[i] = make([]int, LARGEUR_MATRICES)
+		for j := 0; j < LARGEUR_MATRICES; j++ {
+			matB[i][j] = rand.Intn(100) // Default: 10000
+		}
 	}
 }
 
@@ -31,7 +59,7 @@ func printMat(mat [][]int) {
 	fmt.Printf("%s", res)
 }
 
-func printMatList(matList [][][]int, nombre int) {
+/*func printMatList(matList [][][]int, nombre int) {
 	res := ""
 	if nombre > len(matList) {
 		fmt.Printf("Pas assez de matrices dans la liste de matrices pour en afficher %d", nombre)
@@ -49,10 +77,12 @@ func printMatList(matList [][][]int, nombre int) {
 	}
 
 	fmt.Printf("%s", res)
-}
+}*/
 
 func main() {
-	matList := make([][][]int, 3)
+	fmt.Printf("#DEBUG START\n")
+	timeStart := time.Now()
+	/*matList := make([][][]int, 3)
 	sizes := make([][]int, len(matList))
 	for i := 0; i < len(matList); i++ {
 		sizes[i] = make([]int, 3)
@@ -62,15 +92,40 @@ func main() {
 	sizes[1][0] = 30 // taille verticale de la 2ème mat
 	sizes[1][1] = 30 // taille horizontale de la 2ème mat
 	sizes[2][0] = 30
-	sizes[2][1] = 30
+	sizes[2][1] = 30*/
 
-	remplirMatrices(matList, 3, sizes)
-	printMatList(matList, 3)
+	//remplirMatrices(matList, 3, sizes)
+	result = make([][]int, HAUTEUR_MATRICES)
+	remplirMatrices()
+	//printMatList(matList, 3)
+	//printMat(multiplication(matList))
+	printMat(matA)
+	printMat(matB)
 
-	printMat(multiplication(matList))
+	for i := 0; i < HAUTEUR_MATRICES; i++ {
+		wg.Add(1)
+		go multiplicationByLine(i, matA, matB)
+	}
+
+	wg.Wait()
+
+	printMat(result)
+	timeEnd := time.Now()
+	elapsed := timeEnd.Sub(timeStart)
+	fmt.Printf("Time elapsed : %d", elapsed.Milliseconds())
 }
 
-func possibleProduct(rA int, cA int, rB int, cB int) (check bool) {
+func multiplicationByLine(line int, matA [][]int, matB [][]int) {
+	result[line] = make([]int, LARGEUR_MATRICES)
+	for j := 0; j < LARGEUR_MATRICES; j++ {
+		for l := 0; l < HAUTEUR_MATRICES; l++ {
+			result[line][j] = result[line][j] + matA[line][l]*matB[l][j]
+		}
+	}
+	wg.Done()
+}
+
+/*func possibleProduct(rA int, cA int, rB int, cB int) (check bool) {
 	if cA != rB {
 		fmt.Print("Multiplication de matrices impossible")
 		return false
@@ -78,9 +133,9 @@ func possibleProduct(rA int, cA int, rB int, cB int) (check bool) {
 		fmt.Print("Multiplication de matrices possible")
 		return true
 	}
-}
+}*/
 
-func multiplication(matList [][][]int) [][]int {
+/*func multiplication(matList [][][]int) [][]int {
 	var result [][]int
 
 	for k := 1; k < len(matList); k++ {
@@ -97,4 +152,4 @@ func multiplication(matList [][][]int) [][]int {
 	}
 
 	return result
-}
+}*/
